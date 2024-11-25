3f7779a51ccb0f04721a7058a840a4904ad9b682
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2024-11-25 11:30:41 +0200

    Changes

--- backend/routes/userRoute.mjs
+++ backend/routes/userRoute.mjs
@@ -1,5 +1,9 @@
 import express from 'express';
-import { authUser, deleteUser, getAllUsers, getUserProfile, logoutUser, registerUser, updateUserProfile } from '../controllers/userController.mjs';
+import {
+  authUser, deleteUser,
+  getAllUsers, getUserProfile,
+  logoutUser, registerUser, updateUserProfile
+} from '../controllers/userController.mjs';
 import { authorizeRoles, protect } from '../middleware/authMiddleware.mjs';
 const router = express.Router();
 
@@ -7,9 +11,7 @@ router.get('/', protect, authorizeRoles('admin'), getAllUsers);
 router.post('/login', authUser);
 router.post('/register', registerUser);
 router.post('/logout', logoutUser);
-router.route('/profile')
-  .get(protect, getUserProfile)
-  .put(protect, updateUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 router.delete('/:id', protect, authorizeRoles('admin'), deleteUser);
 
 export default router;
